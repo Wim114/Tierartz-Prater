8d38e11a522f5a71975895a1ec669bb01f945830
wim <center.18.24@gmail.com>
2025-08-30 13:39:05 +0200

    Create submit-form.js

---
+++ functions/submit-form.js
@@ -0,0 +1,21 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const data = JSON.parse(event.body);
+  data.access_key = process.env.WEB3FORMS_ACCESS_KEY;
+
+  const response = await fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+
+  const result = await response.json();
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  };
+};
